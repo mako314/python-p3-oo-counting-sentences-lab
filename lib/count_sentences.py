2ff042df33aba32dea8e50e5aa09afb6a796228c
Macolister <bispo.mac@gmail.com>
2023-06-18 13:42:32 -0400

    i dislike these tests

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,53 @@
 #!/usr/bin/env python3
 
-class MyString:
-  pass
+class MyString():
+
+  def __init__(self, value = " "):
+    if isinstance(value, str):
+      self._value = value
+    else:
+      print("The value must be a string.")
+
+  @property
+  def value(self):
+      return self._value
+    
+  @value.setter
+  def value(self, value):
+    if isinstance(value, str):
+      self._value = value
+    print("The value must be a string.")
+
+  def is_sentence(self):
+    suffix = "."
+    return self.value.endswith(suffix)
+    
+    #   return True
+    # else:
+    #   return False
+    
+  def is_question(self):
+    suffix = "?"
+    return self.value.endswith(suffix)
+  
+  def is_exclamation(self):
+    suffix = "!"
+    return self.value.endswith(suffix)
+
+  def count_sentences(self):
+    self.value = self.value.replace("?", ".")
+    self.value = self.value.replace("!", ".", 1)
+    self.value = self.value.replace("...", ".")
+    print(self.value)
+
+    return self.value.count(".")
+
+
+
+print((MyString("This, well, is a sentence. This is too!! And so is this, I think? Woo...").count_sentences()))
+    
+    # if value.endswith("."):
+    #   return True
+    # else:
+    #   return False
+
